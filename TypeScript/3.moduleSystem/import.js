7c0ed9e7c0f40269cbc113212be235468542b2ad
deeksha-kamrani <54739165+deeksha-kamrani@users.noreply.github.com>
2019-11-28 14:49:20 +0530

    Wrong Commit

--- TypeScript/3.moduleSystem/import.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var export_1 = require("./export");
-var person1 = new export_1.Person("deeksha", 23);
-console.log(person1);
--- TypeScript/3.moduleSystem/import.ts
+++
@@ -1,5 +0,0 @@
-import {Person} from './export';
-
-let person1 = new Person("deeksha", 23);
-
-console.log(person1);
